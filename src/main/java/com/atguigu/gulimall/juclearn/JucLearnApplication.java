5e64cd19719416d7a19ca9db003479673cc5b1ae
hongbodang <hongbodang@tencent.com>
2024-11-14 16:09:40 +0800

    Init project

---
+++ src/main/java/com/atguigu/gulimall/juclearn/JucLearnApplication.java
@@ -0,0 +1,13 @@
+package com.atguigu.gulimall.juclearn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JucLearnApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JucLearnApplication.class, args);
+    }
+
+}
